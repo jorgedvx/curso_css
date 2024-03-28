901fdbcd11bcfc64af854c2b955c4b9801ca7180
jorgedvx <jorgecongalopez02@gmail.com>
2024-03-28 16:44:55 -0500

    subido

---
+++ js/questions.js
@@ -0,0 +1,22 @@
+(function(){
+    const titleQuestions = [...document.querySelectorAll('.questions-title')];
+    
+    titleQuestions.forEach(question =>{
+        question.addEventListener('click',()=>{
+            let height = 0;
+            let answer = question.nextElementSibling;
+            let addPadding = question.parentElement.parentElement;
+
+            addPadding.classList.toggle('questions-padding--add')
+
+            question.children[0].classList.toggle('questions-arrow--rotate')
+
+            if(answer.clientHeight === 0){
+                height = answer.scrollHeight;
+            }
+
+            answer.style.height = `${height}px`;
+        })
+    })
+
+})();
